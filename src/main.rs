5520ec282772c55506c48531174a09af77d58209
human9000 <human9000.2021@gmail.com>
2024-11-21 20:36:59 +0500

    changes

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,24 @@ use yandex_music::YandexMusicClient;
 
 mod config;
 mod webserver;
-mod ya_api;
 
 #[tokio::main]
 async fn main() {
-    let rocket_instance = task::spawn(build_rocket());
-    
     let config_path = PathBuf::from("config.toml");
     let config = task::spawn( async {
         parse_config(config_path).await});
     
+    let app_config = config.await.expect("Config error");
+    let client = Arc::new(YandexMusicClient::new(&app_config.keys.ya_music));
+    
+    let rocket_instance = task::spawn(build_rocket(client));
+    
     let rocket_instance = rocket_instance.await.unwrap();
     
-    let handle = task::spawn(async {
+    let handle = task::spawn(async move {
         let _ = rocket_instance.launch().await;
     });
     
-    let app_config = config.await.expect("Config error");
     handle.await;
+    
 }
--- src/webserver.rs
+++ src/webserver.rs
@@ -1,6 +1,37 @@
-use rocket::{fs::FileServer, Build, Rocket};
+use std::sync::Arc;
+use rocket:: State;
 
-pub async fn build_rocket() -> Rocket<Build> {
+use rocket::{fs::FileServer, get, Build, Rocket};
+use serde_json::json;
+use yandex_music::YandexMusicClient;
+
+pub async fn build_rocket(client: Arc<YandexMusicClient>) -> Rocket<Build> {
     rocket::build()
+        .manage(client)
         .mount("/", FileServer::from("static"))
+}
+
+#[get("/song/<track_id>")]
+async fn get_song_by_id(track_id: i32, client: &State<Arc<YandexMusicClient>>) {
+    let download_link = &client.clone().get_track_download_info(track_id).await.unwrap();
+    let download_link = &download_link[0];
+    let download_link = &download_link.download_info_url;
+    
+    let track = &client.get_track(track_id).await.unwrap();
+    let track = &track[0];
+    
+    let track_dur_mins = &track.duration_ms.unwrap() / 1000 / 60;
+    let track_dur_secs = &(track.duration_ms.unwrap() / 1000) - (&track_dur_mins * 60);
+    
+    let track_info = json!({
+        "track_id": track_id,
+        "title": &track.title.unwrap(),
+        "artist": artist,
+        "img": &track.cover_uri.unwrap(),
+        "duration": &track.duration_ms.unwrap() / 1000,
+        "minutes": track_dur_mins,
+        "seconds": track_dur_secs,
+        "album": album,
+        "download_link": download_link
+    });
 }
